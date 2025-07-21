0d5449813aeeabc4f886c5e2491deefcf6527f2d
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-07-21 22:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let number = prompt();
+daysOfAYear(number)
+
+
+
+
+
+
+
+function daysOfAYear(year) {
+
+    if (year % 100 === 0 ? year % 400 === 0 : year % 4 === 0){
+		alert("366");
+	}else{
+		alert("365");
+	}
+}
